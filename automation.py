ca5254c230cb3ce147ccfe61a2ba45faf2739fb2
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-05-16 20:07:51 +0700

    update element

--- automation.py
+++ automation.py
@@ -7,7 +7,7 @@ options.add_experimental_option("detach", True)
 # driver = webdriver.Chrome(options=options)
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(10)
+driver.implicitly_wait(2)
 
 driver.maximize_window()
 
@@ -17,7 +17,5 @@ driver.get('https://www.saucedemo.com/')
 driver.find_element(By.ID,'user-name').send_keys('standard_user')
 driver.find_element(By.ID,'password').send_keys('secret_sauce')
 driver.find_element(By.ID,'login-button').click
-title = driver.find_element(By.XPATH,'//*[@id="header_container"]/div[1]/div[2]/div').text
+title = driver.find_element(By.XPATH,'//div[@classs="app_logo"]').text
 print(title)
-
-print()
