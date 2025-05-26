6d4503696f481236626cfe8beaedd8f23f5d6d07
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-05-26 13:40:48 +0700

    update

--- automation.py
+++ automation.py
@@ -7,7 +7,7 @@ options.add_experimental_option("detach", True)
 # driver = webdriver.Chrome(options=options)
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(2)
+driver.implicitly_wait(5)
 
 driver.maximize_window()
 
@@ -16,6 +16,19 @@ driver.get('https://www.saucedemo.com/')
 
 driver.find_element(By.ID,'user-name').send_keys('standard_user')
 driver.find_element(By.ID,'password').send_keys('secret_sauce')
-driver.find_element(By.ID,'login-button').click
-title = driver.find_element(By.XPATH,'//div[@classs="app_logo"]').text
-print(title)
+driver.find_element(By.ID,'login-button').click()
+driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light").click()
+# driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
+sleep(3)
+# title = driver.find_element(By.XPATH,"//div[@class='app_logo']").text
+# print(title)
+driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+driver.find_element(By.ID, "checkout").click()
+driver.find_element(By.ID, "first-name").send_keys("John")
+driver.find_element(By.ID, "last-name").send_keys("Doe")
+driver.find_element(By.ID, "postal-code").send_keys("12345")
+driver.find_element(By.ID, "continue").click()
+driver.find_element(By.ID, "finish").click()
+confirmation_text = driver.find_element(By.CLASS_NAME, "complete-header").text
+print("Status Checkout:", confirmation_text)
